fe977f1cc2348318c52fa4f4473572d1ab8b9716
eisa godari <eisa.godari@gmail.com>
2023-07-01 22:43:55 +0330

    Comment the config file due to errors while runnign tests

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,138 +1,140 @@
 package config
 
-import (
-	"bufio"
-	"log"
-	"os"
-	"fmt"
-	"strings"
-	"sync"
-)
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"strings"
+// 	"sync"
+// )
 
-// Config Public Interface ---------------------------------------------------------------
-const (
-	PortMain = iota
-	PortMock
+// // Config Public Interface ---------------------------------------------------------------
+// const (
+// 	PortMain = iota
+// 	PortMock
 
-	PostgresUser
-	PostgresPassword
-	PostgresDB
-	PostgresSSL
-	PostgresTimezone
-	DatabasePort
-	DatabaseHost
-	PgadminPW
-	PgadminMail
+// 	PostgresUser
+// 	PostgresPassword
+// 	PostgresDB
+// 	PostgresSSL
+// 	PostgresTimezone
+// 	DatabasePort
+// 	DatabaseHost
+// 	PgadminPW
+// 	PgadminMail
 
-	JwtTokenSecretKey
-	JwtTokenExpireHours
-	EncryptionSecretKey
+// 	JwtTokenSecretKey
+// 	JwtTokenExpireHours
+// 	EncryptionSecretKey
 
-	ReservedParameter
-)
+// 	ReservedParameter
+// )
 
-func Get(envVariableName int) string {
-	if configMap == nil {
-		initConfig()
-	}
-	variable := configMap[envVariableName]
-	if variable == "" {
-		const errFmt = "Environment variable with index:%d is empty or it doesn't exists"
-		errMsg := fmt.Sprintf(errFmt, envVariableName)
-		log.Fatal(errMsg)
-	}
-	return configMap[envVariableName]
-}
+// func Get(envVariableName int) string {
+// 	if configMap == nil {
+// 		initConfig()
+// 	}
+// 	variable := configMap[envVariableName]
+// 	if variable == "" {
+// 		const errFmt = "Environment variable with index:%d is empty or it doesn't exists"
+// 		errMsg := fmt.Sprintf(errFmt, envVariableName)
+// 		log.Fatal(errMsg)
+// 	}
+// 	return configMap[envVariableName]
+// }
 
-// Config Private Implementation ---------------------------------------------------------
-var (
-	configOnce sync.Once
-	configMap  map[int]string
-)
+// // Config Private Implementation ---------------------------------------------------------
+// var (
+// 	configOnce sync.Once
+// 	configMap  map[int]string
+// )
 
-func init() {
-	initEnv()
-	defer initConfig()
-}
+// func init() {
+// 	initEnv()
+// 	defer initConfig()
+// }
 
-func initConfig() {
-	err := loadConfig()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
+// func initConfig() {
+// 	err := loadConfig()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
 
-func loadConfig() error {
-	configOnce.Do(func() {
-		configMap = make(map[int]string)
-		configMap[PortMain] = getEnv("PORT_MAIN")
-		configMap[PortMock] = getEnv("PORT_MOCK")
+// func loadConfig() error {
+// 	configOnce.Do(func() {
+// 		configMap = make(map[int]string)
+// 		configMap[PortMain] = getEnv("PORT_MAIN")
+// 		configMap[PortMock] = getEnv("PORT_MOCK")
 
-		configMap[PostgresUser] = getEnv("POSTGRES_USER")
-		configMap[PostgresPassword] = getEnv("POSTGRES_PASSWORD")
-		configMap[PostgresDB] = getEnv("POSTGRES_DB")
-		configMap[PostgresSSL] = getEnv("POSTGRES_SSL")
-		configMap[PostgresTimezone] = getEnv("POSTGRES_TIMEZONE")
-		configMap[DatabasePort] = getEnv("DATABASE_PORT")
-		configMap[DatabaseHost] = getEnv("DATABASE_HOST")
-		configMap[PgadminPW] = getEnv("PGADMIN_PW")
-		configMap[PgadminMail] = getEnv("PGADMIN_MAIL")
+// 		configMap[PostgresUser] = getEnv("POSTGRES_USER")
+// 		configMap[PostgresPassword] = getEnv("POSTGRES_PASSWORD")
+// 		configMap[PostgresDB] = getEnv("POSTGRES_DB")
+// 		configMap[PostgresSSL] = getEnv("POSTGRES_SSL")
+// 		configMap[PostgresTimezone] = getEnv("POSTGRES_TIMEZONE")
+// 		configMap[DatabasePort] = getEnv("DATABASE_PORT")
+// 		configMap[DatabaseHost] = getEnv("DATABASE_HOST")
+// 		configMap[PgadminPW] = getEnv("PGADMIN_PW")
+// 		configMap[PgadminMail] = getEnv("PGADMIN_MAIL")
 
-		configMap[JwtTokenSecretKey] = getEnv("JWT_TOKEN_SECRET_KEY")
-		configMap[JwtTokenExpireHours] = getEnv("JWT_TOKEN_EXPIRE_HOURS")
-		configMap[EncryptionSecretKey] = getEnv("ENCRYPTION_SECRET_KEY")
+// 		configMap[JwtTokenSecretKey] = getEnv("JWT_TOKEN_SECRET_KEY")
+// 		configMap[JwtTokenExpireHours] = getEnv("JWT_TOKEN_EXPIRE_HOURS")
+// 		configMap[EncryptionSecretKey] = getEnv("ENCRYPTION_SECRET_KEY")
 
-		configMap[ReservedParameter] = getEnv("RESERVED_PARAMETER")
-	})
-	return nil
-}
+// 		configMap[ReservedParameter] = getEnv("RESERVED_PARAMETER")
+// 	})
+// 	return nil
+// }
 
-// Env Private Implementation ------------------------------------------------------------
-const (
-	configFileName        = ".env"
-	separatorOfConfigFile = "="
-)
+// // Env Private Implementation ------------------------------------------------------------
+// const (
+// 	configFileName        = ".env"
+// 	separatorOfConfigFile = "="
+// )
 
-var (
-	envOnce sync.Once
-	envMap  map[string]string
-)
+// var (
+// 	envOnce sync.Once
+// 	envMap  map[string]string
+// )
 
-func initEnv() {
-	err := loadEnvFile()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
+// func initEnv() {
+// 	err := loadEnvFile()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
 
-func getEnv(envName string) string {
-	if envMap == nil {
-		initEnv()
-	}
-	return envMap[envName]
-}
+// func getEnv(envName string) string {
+// 	if envMap == nil {
+// 		initEnv()
+// 	}
+// 	return envMap[envName]
+// }
 
-func loadEnvFile() error {
-	envOnce.Do(func() {
-		envMap = make(map[string]string)
-		envFile, err := os.Open(configFileName)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer envFile.Close()
+// func loadEnvFile() error {
+// 	envOnce.Do(func() {
+// 		envMap = make(map[string]string)
+// 		path, err := os.Getwd()
+// 		println("**************************", path)
+// 		envFile, err := os.Open(configFileName)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		defer envFile.Close()
 
-		scanner := bufio.NewScanner(envFile)
-		for scanner.Scan() {
-			line := scanner.Text()
-			keyVal := strings.Split(line, separatorOfConfigFile)
-			if len(keyVal) != 2 {
-				continue
-			}
-			envMap[keyVal[0]] = keyVal[1]
-		}
-		if err := scanner.Err(); err != nil {
-			log.Fatal(err)
-		}
-	})
-	return nil
-}
+// 		scanner := bufio.NewScanner(envFile)
+// 		for scanner.Scan() {
+// 			line := scanner.Text()
+// 			keyVal := strings.Split(line, separatorOfConfigFile)
+// 			if len(keyVal) != 2 {
+// 				continue
+// 			}
+// 			envMap[keyVal[0]] = keyVal[1]
+// 		}
+// 		if err := scanner.Err(); err != nil {
+// 			log.Fatal(err)
+// 		}
+// 	})
+// 	return nil
+// }
