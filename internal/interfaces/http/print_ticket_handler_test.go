319d70f888e841b2329c489e3af0ada742903b14
eisa godari <eisa.godari@gmail.com>
2023-07-04 12:00:57 +0330

    Add test setup to test case

--- internal/interfaces/http/print_ticket_handler_test.go
+++ internal/interfaces/http/print_ticket_handler_test.go
@@ -5,32 +5,17 @@ import (
 	_ "encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"path"
-	"runtime"
 	"testing"
 
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 	_ "github.com/the-go-dragons/final-project/internal/usecase"
-	"github.com/the-go-dragons/final-project/pkg/config"
 	_ "github.com/the-go-dragons/final-project/pkg/config"
-	"github.com/the-go-dragons/final-project/pkg/database"
+	"github.com/the-go-dragons/final-project/pkg/test"
 )
 
-func init() {
-	_, filename, _, _ := runtime.Caller(0)
-	dir := path.Join(path.Dir(filename), "../../..")
-	config.Path = dir
-}
-
-func Setup() {
-	config.Load()
-	database.Load()
-	database.CreateDBConnection()
-}
-
 func TestTicketHandler(t *testing.T) {
-	Setup()
+	test.Setup()
 	e := echo.New()
 	ticketID := "1"
 	req := httptest.NewRequest(http.MethodGet, "/print_ticket?ticketid="+ticketID, nil)
